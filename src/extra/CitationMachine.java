ced681eec4094ca618e26227b240f166656109d1
Tommy <Tommy@janines-imac.lan>
2018-10-03 21:15:03 -0700

    Citation Machine and Task List

--- src/extra/CitationMachine.java
+++ src/extra/CitationMachine.java
@@ -4,13 +4,12 @@ import javax.swing.JOptionPane;
 
 public class CitationMachine {
 public static void main(String[] args) {
-String Author = JOptionPane.showInputDialog("Type in the author's last name, first name, and middle name (if any).");
-String Atitle = JOptionPane.showInputDialog("Type in the article title in quotes.");
+String Author = JOptionPane.showInputDialog("Type in the author's last name and first name with a comma before the last name.");
+String Atitle = JOptionPane.showInputDialog("Type in the article title in quotes with a period before the end quotes.");
 String Wtitle = JOptionPane.showInputDialog("Type in the website title.");
-String Publisher = JOptionPane.showInputDialog("Type in the publisher/sponsor.");
-String DateP = JOptionPane.showInputDialog("Type in when the website was published.");
+String Page = JOptionPane.showInputDialog("Type in the page or paragraph number that you got the information from. Use the abbreviation p. for page and par. for paragraph.");
 String URL = JOptionPane.showInputDialog("Type in the URL.");
 String DateA = JOptionPane.showInputDialog("Type in when you accessed the website.");
-JOptionPane.showMessageDialog(null, Author + ". " + Atitle + ". " + Wtitle + ", " + Publisher + "," + '\n' + DateP + ", " + URL + ". " + "Accessed " + DateA + ". ");
+JOptionPane.showMessageDialog(null, Author + ". " + Atitle + Wtitle + ", " + Page + "," + '\n' + URL + ". " + "Accessed " + DateA + ". ");
 }
 }
---
+++ src/extra/TaskList.java
@@ -0,0 +1,12 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class TaskList {
+public static void main(String[] args) {
+String Ftask = JOptionPane.showInputDialog("Type in your first task.");
+String Stask = JOptionPane.showInputDialog("Type in your second task.");
+String Ttask = JOptionPane.showInputDialog("Type in your third task.");
+String FOtask = JOptionPane.showInputDialog("Type in your fourth task.");
+}
+}
